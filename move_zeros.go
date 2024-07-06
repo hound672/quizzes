bbe2e02ebeeb5d54226443c20d876b09b5089254
hound672 <hound672@gmail.com>
2024-07-06 13:09:50 +0300

    move zero

--- move_zeros.go
+++ move_zeros.go
@@ -3,7 +3,7 @@ package main
 
 import "fmt"
 
-func MoveZeros(arr []int) []int {
+func MoveZerosMem(arr []int) []int {
 	res := make([]int, len(arr))
 	pos := 0
 
@@ -17,7 +17,26 @@ func MoveZeros(arr []int) []int {
 	return res
 }
 
+func MoveZeros(arr []int) []int {
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 0 {
+			for j := i + 1; j < len(arr); j++ {
+				if arr[j] == 0 {
+					continue
+				}
+				arr[i] = arr[j]
+				arr[j] = 0
+				//i++
+				i = j + 1
+			}
+		}
+	}
+
+	return arr
+}
+
 func main() {
-	res := MoveZeros([]int{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) // To(Equal([]int{ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 })
+	res := MoveZeros([]int{1, 2, 0, 1, 0, 0, 2, 0, 1, 0, 3, 0, 1}) // To(Equal([]int{ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 })
 	fmt.Printf("res: %v\n", res)
 }
